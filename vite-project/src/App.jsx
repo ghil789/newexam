4b84de892a64a57125fd3b5242743c3fd4f61477
yodahe1 <yodahemeless@gmail.com>
2024-11-02 11:26:49 +0300

    create the pages using react router dom home page, signin page ,signup page and dashboard for the admin

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,3 +1,22 @@
-export default function App() {
-  return <h1 className="text-3xl text-red-500">App</h1>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Dashboard from "./pages/dashboard";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
