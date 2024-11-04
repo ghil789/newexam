320978ba7e856dfacc06fcfc54877b7df9c8c3a7
yodahe1 <yodahemeless@gmail.com>
2024-11-04 11:46:06 +0300

    at this level of the project's progress the navigation bar is sucsesfully created and using flowbite dependency its responsive for every screen size

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,6 +1,16 @@
-import { Navbar, TextInput } from "flowbite-react";
+import {
+  Button,
+  Navbar,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+  TextInput,
+} from "flowbite-react";
 import { IoIosSearch } from "react-icons/io";
+import { IoIosMoon } from "react-icons/io";
+import { useLocation } from "react-router-dom";
 const Header = () => {
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <h1>
@@ -21,8 +31,33 @@ const Header = () => {
           type="text"
           placeholder="Search course here"
           rightIcon={IoIosSearch}
+          className="hidden lg:inline"
         />
       </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <IoIosSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <IoIosMoon />
+        </Button>
+        <a href="/Signin">
+          <Button gradientDuoTone="purpleToBlue">Sign in</Button>
+        </a>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink active={path === "/"} as={"div"}>
+          <a href="/">home</a>
+        </NavbarLink>
+        <NavbarLink active={path === "/About"} as={"div"}>
+          <a href="/About">about</a>
+        </NavbarLink>
+
+        <NavbarLink active={path === "/Dashboard"} as={"div"}>
+          <a href="/Dashboard">dashboard</a>
+        </NavbarLink>
+      </NavbarCollapse>
     </Navbar>
   );
 };
