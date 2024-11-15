64c8b7b8f3cc596f14a7d89e95c48f76bf340925
yodahe1 <yodahemeless@gmail.com>
2024-11-15 16:59:10 +0300

    create sign in api router

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const signup = async (req,res,next) => {
@@ -26,3 +27,31 @@ export const signup = async (req,res,next) => {
 
     
 };
+export const signin = async(req, res, next) =>{
+    const  {email, password} = req.body;
+
+    if (!email||!password || email === '' || password === '') {
+        next(errorHandler(400,'all fields are required'));}
+        try {
+            const validUser = await User.findOne({email});
+            if (!validUser) {
+                return next(errorHandler(404,'user not found'));
+            }
+            const validPassword = await User.findOne({password});
+            if (!validPassword) {
+                return next(errorHandler(404,'invalid password'));
+
+        }
+        const token = jwt.sign(
+            {id:validUser._id},process.env.JWT_SECRET);
+            const {password:pass, ...rest} = validUser._doc;
+            res.status(200).cookie('access_token', token,{httpOnly:true,}).json(rest);
+    } 
+
+        catch (error){
+            next(error);
+        }
+
+
+    };
+
